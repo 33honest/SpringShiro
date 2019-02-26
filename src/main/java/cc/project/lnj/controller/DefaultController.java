8da3b2837d4173844a345012ca73f434810a3634
33honest <411607562@qq.com>
2019-02-26 15:27:15 +0800

    优化pom

--- src/main/java/cc/project/lnj/controller/DefaultController.java
+++ src/main/java/cc/project/lnj/controller/DefaultController.java
@@ -1,8 +1,12 @@
 package cc.project.lnj.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class DefaultController {
@@ -14,5 +18,11 @@ public class DefaultController {
         return "Hello WOrld";
     }
 
+    @RequestMapping("/welcome.html")
+    public String welcomeAction(HttpServletRequest req, HttpServletResponse rep, ModelMap map) {
+
+        map.put("name", "admin");
+        return "welcome";
+    }
 
 }
